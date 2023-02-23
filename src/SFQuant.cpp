e403d43934a212ecbd7079bf5a212461a5c26697
K-Magara <magara@maizuru-ct.ac.jp>
2023-02-23 16:18:08 +0900

    SFQuant

--- src/SFQuant.cpp
+++ src/SFQuant.cpp
@@ -17,15 +17,14 @@ SFQuant::SFQuant()
 // u,v - (u, v)パラメータ
 SFQuant::SFQuant(const NURBSS* S, double u, double v)
 {
-	NURBS_Func nf;
 	U = u;
 	V = v;
-	Coord du = nf.CalcDiffuNurbsS(S,u,v);			// u方向1階微分
-	Coord dv = nf.CalcDiffvNurbsS(S,u,v);			// v方向1階微分
-	Coord duu = nf.CalcDiffNNurbsS(S,2,0,u,v);		// u方向2階微分
-	Coord dvv = nf.CalcDiffNNurbsS(S,0,2,u,v);		// v方向2階微分
-	Coord duv = nf.CalcDiffNNurbsS(S,1,1,u,v);		// u,v方向各1階微分
-	n = nf.CalcNormVecOnNurbsS(S,u,v);				// 法線ベクトル
+	Coord du = S->CalcDiffuNurbsS(u,v);			// u方向1階微分
+	Coord dv = S->CalcDiffvNurbsS(u,v);			// v方向1階微分
+	Coord duu = S->CalcDiffNNurbsS(2,0,u,v);	// u方向2階微分
+	Coord dvv = S->CalcDiffNNurbsS(0,2,u,v);	// v方向2階微分
+	Coord duv = S->CalcDiffNNurbsS(1,1,u,v);	// u,v方向各1階微分
+	n = S->CalcNormVecOnNurbsS(u,v);			// 法線ベクトル
 	E = du.CalcInnerProduct(du);				// 第1基本量
 	F = du.CalcInnerProduct(dv);				// 第1基本量
 	G = dv.CalcInnerProduct(dv);				// 第1基本量
@@ -43,15 +42,14 @@ SFQuant::SFQuant(const NURBSS* S, double u, double v)
 // u,v - (u, v)パラメータ
 int SFQuant::SetSFQ(const NURBSS* S, double u, double v)
 {	
-	NURBS_Func nf;
 	U = u;
 	V = v;
-	Coord du = nf.CalcDiffuNurbsS(S,u,v);			// u方向1階微分
-	Coord dv = nf.CalcDiffvNurbsS(S,u,v);			// v方向1階微分
-	Coord duu = nf.CalcDiffNNurbsS(S,2,0,u,v);		// u方向2階微分
-	Coord dvv = nf.CalcDiffNNurbsS(S,0,2,u,v);		// v方向2階微分
-	Coord duv = nf.CalcDiffNNurbsS(S,1,1,u,v);		// u,v方向各1階微分
-	n = nf.CalcNormVecOnNurbsS(S,u,v);				// 法線ベクトル
+	Coord du = S->CalcDiffuNurbsS(u,v);			// u方向1階微分
+	Coord dv = S->CalcDiffvNurbsS(u,v);			// v方向1階微分
+	Coord duu = S->CalcDiffNNurbsS(2,0,u,v);	// u方向2階微分
+	Coord dvv = S->CalcDiffNNurbsS(0,2,u,v);	// v方向2階微分
+	Coord duv = S->CalcDiffNNurbsS(1,1,u,v);	// u,v方向各1階微分
+	n = S->CalcNormVecOnNurbsS(u,v);			// 法線ベクトル
 	E = du.CalcInnerProduct(du);				// 第1基本量
 	F = du.CalcInnerProduct(dv);				// 第1基本量
 	G = dv.CalcInnerProduct(dv);				// 第1基本量
@@ -70,11 +68,10 @@ int SFQuant::SetSFQ(const NURBSS* S, double u, double v)
 // u,v - (u, v)パラメータ
 int SFQuant::SetSFQ1(const NURBSS* S, double u, double v)
 {
-    NURBS_Func nf;
     U = u;
     V = v;
-    Coord du = nf.CalcDiffuNurbsS(S,u,v);			// u方向1階微分
-    Coord dv = nf.CalcDiffvNurbsS(S,u,v);			// v方向1階微分
+    Coord du = S->CalcDiffuNurbsS(u,v);			// u方向1階微分
+    Coord dv = S->CalcDiffvNurbsS(u,v);			// v方向1階微分
     E = du.CalcInnerProduct(du);				// 第1基本量
     F = du.CalcInnerProduct(dv);				// 第1基本量
     G = dv.CalcInnerProduct(dv);				// 第1基本量
